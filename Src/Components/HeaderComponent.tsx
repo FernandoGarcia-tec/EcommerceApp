05dc24e9cb635e1334c82617b2587ddf38ba3381
Fernando <20460234@colima.tecnm.mx>
2025-05-17 18:27:14 -0600

    icono cart

--- Src/Components/HeaderComponent.tsx
+++ Src/Components/HeaderComponent.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
 import React, { useState } from 'react';
-import { Entypo, AntDesign, Ionicons } from "@expo/vector-icons"
+import { Entypo, AntDesign, Ionicons, MaterialIcons } from "@expo/vector-icons"
+import { GoBack } from './GoBackNavButton';
 
 interface IHeaderParams {
     goToPrevios?: () => void;
@@ -15,6 +16,7 @@ export const HeaderComponent = ({ goToPrevios, search, cartlenght, goToCartScree
     return (
         // Este componente es un encabezado que contiene un campo de búsqueda 
         <View style={{ backgroundColor: "#000", padding: 10, flexDirection: "row", alignItems: "center" }}>
+            <GoBack onPress={goToPrevios}/>
             {/* campo de busqueda */}
             <Pressable style={{ flexDirection: "row", alignItems: "center", marginHorizontal: 7, gap: 10, backgroundColor: "white", borderRadius: 10, height: 38, flex: 1 }}>
                 {/* Icono de búsqueda */}
@@ -30,7 +32,31 @@ export const HeaderComponent = ({ goToPrevios, search, cartlenght, goToCartScree
                     placeholder="Search Item"
                     placeholderTextColor="#888"/>            
             </Pressable>
-
+            {/* Icono de carrito */}
+            <Pressable onPress={goToCartScreen}>
+                <View style={styles.cartNum}>
+                    <Text style={{color:"white"}}>{cartlenght}</Text>
+                </View>
+                <MaterialIcons name="shopping-cart" size={24} color="white" style={{padding:5, marginTop:3}} />
+            </Pressable>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    cartNum: {
+        width: 20,
+        height: 20,
+        backgroundColor: "orange",
+        borderRadius: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        right: -5,
+        top: -5,
+        zIndex: 9999,
+        color:"#fff",
+        
+    }
+})
--- Src/Screens/HomeScreens.tsx
+++ Src/Screens/HomeScreens.tsx
@@ -1,13 +1,20 @@
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import react from "react";
-import { View, Text, SafeAreaView, Platform } from "react-native";
+import { View, Text, SafeAreaView, Platform, ScrollView } from "react-native";
 import { TabStackScreenProps } from "../Navigations/TabNavigator";
 import { HeaderComponent } from "../Components/HeaderComponent";
 
 const HomeScreen = ({navigation,route}:TabStackScreenProps<"Home">) => {
+    const gotoCartScreen = () => {
+        navigation.navigate("cart");
+    }
     return (
         <SafeAreaView style={{ padding: Platform.OS === "android" ? 40 : 0, flex: 1, backgroundColor: "#000" }}>
-            <HeaderComponent />
+            <HeaderComponent goToCartScreen={gotoCartScreen}/>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} 
+                style={{ backgroundColor: "#eee" }}>
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
